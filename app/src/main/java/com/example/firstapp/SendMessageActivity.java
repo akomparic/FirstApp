14e015eed24c9aed22b2039f48ae624f7f465e5d
Aleksandar Komparic <lordkilllalot@gmail.com>
2014-09-10 13:33:58 +0200

    App works with basic authentication and hardcoded values

--- app/src/main/java/com/example/firstapp/SendMessageActivity.java
+++ app/src/main/java/com/example/firstapp/SendMessageActivity.java
@@ -4,10 +4,21 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.lang.String;
 
 
 public class SendMessageActivity extends Activity {
@@ -17,7 +28,7 @@ public class SendMessageActivity extends Activity {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         String filename = "SendTextStorage";
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        final String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         FileOutputStream outputStream;
 
@@ -29,6 +40,40 @@ public class SendMessageActivity extends Activity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        Thread t = new Thread() {
+            String user = getString(R.string.username);
+            String password = getString(R.string.password);
+
+            public void run() {
+                Looper.prepare(); //For Preparing Message Pool for the child Thread
+                HttpClient client = new DefaultHttpClient();
+                HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+                HttpResponse response;
+                JSONObject json = new JSONObject();
+
+                try {
+                    HttpPut put = new HttpPut("http://192.168.232.1:8050/auth");
+                    json.put("user", user);
+                    json.put("pass", password);
+                    json.put("content", message);
+                    StringEntity se = new StringEntity( json.toString());
+                    se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                    put.setEntity(se);
+                    response = client.execute(put);
+
+                    /*Checking response */
+                    if(response!=null){
+                        InputStream in = response.getEntity().getContent(); //Get the data in the entity
+                    }
+
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    }
+
+                Looper.loop(); //Loop in the message queue
+            }
+        };
+        t.start();
     }
 
     @Override
